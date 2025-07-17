914f2bdcfd02fafedbc476c79fa9efea8741920a
000begana000 <begana.contact@gmail.com>
2025-07-17 19:20:19 +0200

    nknown type

---
+++ 02-essential/unknown.ts
@@ -0,0 +1,12 @@
+// any allows anything - no need if check
+// unknown forces us to add some if checks
+function process(val: unknown) {
+  if (
+    typeof val === "object" &&
+    !!val &&
+    "log" in val &&
+    typeof val.log === "function"
+  ) {
+    val.log();
+  }
+}
